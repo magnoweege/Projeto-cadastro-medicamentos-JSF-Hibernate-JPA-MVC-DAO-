f8b293f173e181e021131d9db6322d1eef708cf3
Magno Weege <magnoo@gmail.com>
2021-09-12 00:44:32 -0300

    mudanças dos estudos

---
+++ src/main/java/br/com/medicamento/model/Medicamento.java
@@ -0,0 +1,89 @@
+package br.com.medicamento.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Medicamento")
+public class Medicamento implements Serializable, Base{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue
+	@Column(name="codigo")
+	private Long id;
+	private String nome;
+	private String apresentacao;
+	private String laboratorio;
+	private String registro;
+	private BigDecimal preco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getApresentacao() {
+		return apresentacao;
+	}
+	public void setApresentacao(String apresentacao) {
+		this.apresentacao = apresentacao;
+	}
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+	public String getRegistro() {
+		return registro;
+	}
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medicamento other = (Medicamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
